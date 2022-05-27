9e2bbb690db407ade550a6824176f5140d941574
salteadorneo <hola@salteadorneo.dev>
2022-05-27 02:43:59 +0200

    css responsive + random

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,20 @@
       <img :src="'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/' + (pokemon.id) + '.png'" :class="{ 'pokemon': true, 'active': win }" />
     </div>
 
+    <autocomplete :search="search" placeholder="¿Qué Pokemon es?" @submit="intent" />
+
+    <br />
     <h3>Intentos</h3>
     <div class="pokeballs">
       <img v-for="item in intents" v-bind:key="item" src="./assets/pokeball.png" class="pokeball" />
     </div>
 
-    <autocomplete :search="search" placeholder="¿Qué Pokemon es?" @submit="intent"></autocomplete>
+    <br /><br /><br /><br />
 
     <h1>Pokédex</h1>
 
-    <div class="pokemones">
-      <div v-for="(pokemon, index) in pokemones" v-bind:key="index" :class="{ 'pokemon': true, 'active': pokemon.active }">
+    <div class="pokedex">
+      <div v-for="(pokemon, index) in pokedex" v-bind:key="index" :class="{ 'pokemon': true, 'active': pokemon.active }">
         <img :src="'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/' + (index + 1) + '.png'" />
         <h2>{{('000'+(index+1)).slice(-3)}} {{ pokemon.name }}</h2>
       </div>
@@ -28,44 +31,53 @@ export default {
   name: 'App',
   data() {
     return {
-      pokemones: localStorage.pokemones ? JSON.parse(localStorage.pokemones) : [],
+      pokedex: localStorage.pokedex ? JSON.parse(localStorage.pokedex) : [],
       pokemon: {},
       intents: 5,
-      win: false
+      win: false,
+    focused: false,
+    value: '',
+    results: []
+    }
+  },
+  computed: {
+    noResults() {
+      return this.value && this.results.length === 0
     }
   },
   components: {
   },
   mounted () {
-    if (!localStorage.pokemones) {
+    if (!localStorage.pokedex) {
       axios.get('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0').then(res => {
-        this.pokemones = res.data.results
+        this.pokedex = res.data.results
 
-        localStorage.pokemones = JSON.stringify(this.pokemones)
+        localStorage.pokedex = JSON.stringify(this.pokedex)
+
+        this.randomPokemon()
       })
-    }
-    
-    const random = Math.floor(Math.random() * this.pokemones.length)
-    this.pokemon = this.pokemones[random]
-    this.pokemon.id = random + 1
-
-    console.log(this.pokemon.name)
-
-    /*axios.get(pokemon.url).then(res => {
-        let data = res.data
-        this.image = data.sprites.other["official-artwork"].front_default
-      })*/
+    } else this.randomPokemon()
   },
   methods: {
+    randomPokemon() {
+      this.win = false
+      this.intents = 5
+
+      const random = Math.floor(Math.random() * this.pokedex.length)
+      this.pokemon = this.pokedex[random]
+      this.pokemon.id = random + 1
+
+      console.log(this.pokemon.name)
+    },
     search(input) {
       if (input.length < 1) { return [] }
-      return this.pokemones.filter(i => { return i.name.toLowerCase().startsWith(input.toLowerCase())}).map(i => { return i.name })
+      return this.pokedex.filter(i => { return i.name.toLowerCase().startsWith(input.toLowerCase())}).map(i => { return i.name })
     },
     intent(e) {
       if (e == this.pokemon.name) {
         this.win = true
         this.pokemon.active = true
-        localStorage.pokemones = JSON.stringify(this.pokemones)
+        localStorage.pokedex = JSON.stringify(this.pokedex)
       } else {
         this.intents--
       }
@@ -96,10 +108,13 @@ h3 {
 
 .scene {
   width: 100%;
-  height: 475px;
+  height: 50vw;
+  max-height: 475px;
   background: url(./assets/back.jpg) no-repeat center / cover;
+  margin: 30px auto;
 
   .pokemon {
+    max-width: 50%;
     filter: brightness(0);
     transition: all 1s;
 
@@ -117,7 +132,7 @@ h3 {
   }
 }
 
-.pokemones {
+.pokedex {
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
@@ -127,6 +142,18 @@ h3 {
     flex-basis: 10%;
     font-size: 9px;
     text-align: center;
+
+    @media (max-width: 800px) {
+      flex-basis: 20%;
+    }
+
+    @media (max-width: 680px) {
+      flex-basis: 25%;
+    }
+
+    @media (max-width: 480px) {
+      flex-basis: 33%;
+    }
 
     &.active {
       img {
