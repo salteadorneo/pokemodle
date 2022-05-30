cb45883dc957792501989e4cfbf84016824ab631
Cristian Adán <cristian@doersdf.com>
2022-05-30 17:15:08 +0200

    keyboard css + pokedex png

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="app">
-    <div class="scene">
+    <div class="scene" v-if="pokemon.id">
       <img :src="'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/' + (pokemon.id) + '.png'" :class="{ 'pokemon': true, 'active': win }" />
     </div>
 
-    <autocomplete :search="search" placeholder="¿Qué Pokemon es?" @submit="intent" />
+    <input v-model="input" placeholder="¿Qué Pokemon es?" readonly />
 
     <br />
     <h3>Intentos</h3>
@@ -14,8 +14,6 @@
 
     <br /><br /><br /><br />
 
-    <h1>Pokédex</h1>
-
     <div class="pokedex">
       <div v-for="(pokemon, index) in pokedex" v-bind:key="index" :class="{ 'pokemon': true, 'active': pokemon.active }">
         <img :src="'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/' + (index + 1) + '.png'" />
@@ -24,46 +22,44 @@
     </div>
 
     <div class="keyboard">
-      <div>
-        <div class="keyrow">
-          <button type="button" class="key" aria-label="q">q</button>
-          <button type="button" class="key" aria-label="w">w</button>
-          <button type="button" class="key" aria-label="e">e</button>
-          <button type="button" class="key" aria-label="r">r</button>
-          <button type="button" class="key" aria-label="t">t</button>
-          <button type="button" class="key" aria-label="y">y</button>
-          <button type="button" class="key" aria-label="u">u</button>
-          <button type="button" class="key" aria-label="i">i</button>
-          <button type="button" class="key" aria-label="o">o</button>
-          <button type="button" class="key" aria-label="p">p</button>
-        </div>
-        <div class="keyrow">
-          <button type="button" class="key" aria-label="a">a</button>
-          <button type="button" class="key" aria-label="s">s</button>
-          <button type="button" class="key" aria-label="d">d</button>
-          <button type="button" class="key" aria-label="f">f</button>
-          <button type="button" class="key" aria-label="g">g</button>
-          <button type="button" class="key" aria-label="h">h</button>
-          <button type="button" class="key" aria-label="j">j</button>
-          <button type="button" class="key" aria-label="k">k</button>
-          <button type="button" class="key" aria-label="l">l</button>
-          <button type="button" class="key" aria-label="ñ">ñ</button>
-        </div>
-        <div class="keyrow">
-          <button type="button" class="key" aria-label="enviar">enviar</button>
-          <button type="button" class="key" aria-label="z">z</button>
-          <button type="button" class="key" aria-label="x">x</button>
-          <button type="button" class="key" aria-label="c">c</button>
-          <button type="button" class="key" aria-label="v">v</button>
-          <button type="button" class="key" aria-label="b">b</button>
-          <button type="button" class="key" aria-label="n">n</button>
-          <button type="button" class="key" aria-label="m">m</button>
-          <button type="button" class="key" aria-label="borrar letra">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true" style="width:1.5rem;height:1.5rem">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2M3 12l6.414 6.414a2 2 0 001.414.586H19a2 2 0 002-2V7a2 2 0 00-2-2h-8.172a2 2 0 00-1.414.586L3 12z"></path>
-            </svg>
-          </button>
-        </div>
+      <div class="keyrow">
+        <button type="button" class="key" @click="setKey('q')">q</button>
+        <button type="button" class="key" @click="setKey('w')">w</button>
+        <button type="button" class="key" @click="setKey('e')">e</button>
+        <button type="button" class="key" @click="setKey('r')">r</button>
+        <button type="button" class="key" @click="setKey('t')">t</button>
+        <button type="button" class="key" @click="setKey('y')">y</button>
+        <button type="button" class="key" @click="setKey('u')">u</button>
+        <button type="button" class="key" @click="setKey('i')">i</button>
+        <button type="button" class="key" @click="setKey('o')">o</button>
+        <button type="button" class="key" @click="setKey('p')">p</button>
+      </div>
+      <div class="keyrow">
+        <button type="button" class="key" @click="setKey('a')">a</button>
+        <button type="button" class="key" @click="setKey('s')">s</button>
+        <button type="button" class="key" @click="setKey('d')">d</button>
+        <button type="button" class="key" @click="setKey('f')">f</button>
+        <button type="button" class="key" @click="setKey('g')">g</button>
+        <button type="button" class="key" @click="setKey('h')">h</button>
+        <button type="button" class="key" @click="setKey('j')">j</button>
+        <button type="button" class="key" @click="setKey('k')">k</button>
+        <button type="button" class="key" @click="setKey('l')">l</button>
+        <button type="button" class="key" @click="setKey('ñ')">ñ</button>
+      </div>
+      <div class="keyrow">
+        <button type="button" class="key" @click="setKey('enter')">enviar</button>
+        <button type="button" class="key" @click="setKey('z')">z</button>
+        <button type="button" class="key" @click="setKey('x')">x</button>
+        <button type="button" class="key" @click="setKey('c')">c</button>
+        <button type="button" class="key" @click="setKey('v')">v</button>
+        <button type="button" class="key" @click="setKey('b')">b</button>
+        <button type="button" class="key" @click="setKey('n')">n</button>
+        <button type="button" class="key" @click="setKey('m')">m</button>
+        <button type="button" class="key" @click="setKey('return')">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true" style="width:1.5rem;height:1.5rem">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2M3 12l6.414 6.414a2 2 0 001.414.586H19a2 2 0 002-2V7a2 2 0 00-2-2h-8.172a2 2 0 00-1.414.586L3 12z"></path>
+          </svg>
+        </button>
       </div>
     </div>
 
@@ -80,15 +76,10 @@ export default {
       pokemon: {},
       intents: 5,
       win: false,
-    focused: false,
-    value: '',
-    results: []
+      input: '',
     }
   },
   computed: {
-    noResults() {
-      return this.value && this.results.length === 0
-    }
   },
   components: {
   },
@@ -102,15 +93,36 @@ export default {
         this.randomPokemon()
       })
     } else this.randomPokemon()
+
+     document.body.addEventListener("keyup", this.keyup) 
   },
   methods: {
+    setKey(v) {
+      if (v === 'enter') {
+        this.checkPokemon(this.input)
+      } else if (v === 'return') {
+        this.input = this.input.slice(0, -1)
+      } else {
+        this.input += v
+      }
+    },
+    keyup(e) {
+      if (e.key === 'Enter') {
+        this.checkPokemon(this.input)
+      } else if (e.key === 'Backspace') {
+        this.input = this.input.slice(0, -1)
+      } else {
+        this.input += e.key
+      }
+    },
     randomPokemon() {
       this.win = false
       this.intents = 5
 
-      const random = Math.floor(Math.random() * this.pokedex.length)
-      this.pokemon = this.pokedex[random]
-      this.pokemon.id = random + 1
+      let noActives = this.pokedex.filter(i => !i.active)
+
+      this.pokemon = noActives[Math.floor(Math.random() * noActives.length)]
+      this.pokemon.id = this.pokemon.url.split('/')[6]
 
       console.log(this.pokemon.name)
     },
@@ -118,14 +130,15 @@ export default {
       if (input.length < 1) { return [] }
       return this.pokedex.filter(i => { return i.name.toLowerCase().startsWith(input.toLowerCase())}).map(i => { return i.name })
     },
-    intent(e) {
-      if (e == this.pokemon.name) {
+    checkPokemon(e) {
+      if (e.toLowerCase().trim() == this.pokemon.name.toLowerCase().trim()) {
         this.win = true
         this.pokemon.active = true
         localStorage.pokedex = JSON.stringify(this.pokedex)
       } else {
         this.intents--
       }
+      this.input = ''
       if (this.intents == 0) location.reload()
     }
   }
@@ -143,7 +156,7 @@ body {
 
 #app {
   width: 820px;
-  max-width: 90%;
+  max-width: 100%;
   margin: 0 auto;
 }
 
@@ -153,12 +166,15 @@ h3 {
 
 .scene {
   width: 100%;
-  height: 50vw;
+  height: 40vw;
   max-height: 475px;
   background: url(./assets/back.jpg) no-repeat center / cover;
-  margin: 30px auto;
+  margin: 0 auto;
 
   .pokemon {
+    position: relative;
+    top: 3%;
+    left: 5%;
     max-width: 50%;
     filter: brightness(0);
     transition: all 1s;
@@ -177,16 +193,25 @@ h3 {
   }
 }
 
+input {
+  width: 100%;
+  line-height: 10vh;
+}
+
 .pokedex {
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
   align-items: flex-end;
+  background: url(./assets/pokedex.png) no-repeat center top #ed1e24;
+  background-size: contain;
+  padding-top: 100px;
 
   & > div {
     flex-basis: 10%;
     font-size: 9px;
     text-align: center;
+    color: white;
 
     @media (max-width: 800px) {
       flex-basis: 20%;
@@ -218,22 +243,29 @@ h3 {
 
 .keyboard {
   position: fixed;
-  left: 0;
+  left: 50%;
   bottom: 0;
+  transform: translateX(-50%);
   width: 100%;
+  max-width: 492px;
+  max-height: 40vh;
   text-align: center;
 
-  .key {
-    display: inline-block;
-    vertical-align: top;
-    color: white;
-    background: #333;
-    border-radius: 4px;
-    width: 45px;
-    height: 56px;
-    outline: none;
-    border: 0;
-    margin: 1px;
+  .keyrow {
+    display: flex;
+    justify-content: center;
+    width: 100%;
+
+    .key {
+      flex: auto;
+      color: white;
+      background: #333;
+      border-radius: 6px;
+      height: 56px;
+      outline: none;
+      border: 0;
+      margin: 2px;
+    }
   }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Autocomplete from '@trevoreyre/autocomplete-vue'
-import '@trevoreyre/autocomplete-vue/dist/style.css'
-
-Vue.use(Autocomplete)
 
 Vue.config.productionTip = false
 
