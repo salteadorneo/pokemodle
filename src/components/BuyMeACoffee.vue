0a0f22a5f752890a1674f617bd4a442694602766
salteadorneo <hola@salteadorneo.dev>
2022-10-25 21:43:52 +0200

    add "BuyMeACoffee" button

---
+++ src/components/BuyMeACoffee.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="buymeacoffee">
+    <a href="https://www.buymeacoffee.com/salteadorneo" target="_blank"
+      ><img
+        src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
+        alt="Buy Me A Coffee"
+        style="height: 50px !important; width: 178px !important"
+    /></a>
+  </div>
+</template>
+
+<style scoped>
+.buymeacoffee {
+  position: fixed;
+  bottom: 5px;
+  right: 5px;
+  z-index: 999;
+}
+@media (max-width: 950px) {
+  .buymeacoffee {
+    display: none;
+  }
+}
+</style>
