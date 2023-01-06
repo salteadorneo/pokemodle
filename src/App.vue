66714bfc18d08aad6556502aa256742f6fe7129a
salteadorneo <hola@salteadorneo.dev>
2023-01-06 23:25:21 +0100

    add test local notifications

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,9 @@
 
     <img src="./assets/logo.png" alt="Pokemodle" class="logo" />
 
+    <button class="add-button">Add to home screen</button>
+    <button id="notifications">Notif</button>
+
     <div class="scene" v-if="pokemon.id">
       <img
         :src="
@@ -135,6 +138,53 @@ export default {
           this.randomPokemon();
         });
     } else this.randomPokemon();
+
+    let deferredPrompt;
+    const addBtn = document.querySelector(".add-button");
+    //addBtn.style.display = "none";
+    window.addEventListener("beforeinstallprompt", (e) => {
+      // Prevent Chrome 67 and earlier from automatically showing the prompt
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      deferredPrompt = e;
+      // Update UI to notify the user they can add to home screen
+      addBtn.style.display = "block";
+
+      addBtn.addEventListener("click", () => {
+        // hide our user interface that shows our A2HS button
+        addBtn.style.display = "none";
+        // Show the prompt
+        deferredPrompt.prompt();
+        // Wait for the user to respond to the prompt
+        deferredPrompt.userChoice.then((choiceResult) => {
+          if (choiceResult.outcome === "accepted") {
+            console.log("User accepted the A2HS prompt");
+          } else {
+            console.log("User dismissed the A2HS prompt");
+          }
+          deferredPrompt = null;
+        });
+      });
+    });
+
+    const button = document.getElementById("notifications");
+    button.addEventListener("click", () => {
+      Notification.requestPermission().then((result) => {
+        if (result === "granted") {
+          randomNotification({ title: "test" });
+        }
+      });
+      function randomNotification({ title }) {
+        //const randomItem = Math.floor(Math.random() * 1);
+        const notifBody = `Es hora de atraparlo`;
+        //const notifImg = `data/img/${games[randomItem].slug}.jpg`;
+        const options = {
+          body: notifBody,
+          //icon: notifImg,
+        };
+        new Notification(title, options);
+      }
+    });
 
     document.body.addEventListener("keyup", this.keyup);
   },
